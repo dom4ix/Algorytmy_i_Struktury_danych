31a7129466f60a62f5ae9be73d2213ca311f3bae
dom4ix <47005404+dom4ix@users.noreply.github.com>
2022-10-25 19:57:06 +0300

    Update Zad_1_Tablice.c

--- laboratoria/Lab_2/Zad_1_Tablice.c
+++ laboratoria/Lab_2/Zad_1_Tablice.c
@@ -61,3 +61,31 @@
     //int find(int *arr);
 
     // Funkcii
+
+
+
+
+#include <stdio.h>
+ 
+int main() {
+    int arr[] = {2, 4, 6, 8, 10};
+    int x = 8;
+     
+    int arrLen = sizeof arr / sizeof arr[0];
+    int isElementPresent = 0;
+     
+    for (int i = 0; i < arrLen; i++) {
+        if (arr[i] == x) {
+            isElementPresent = 1;
+            break;
+        }
+    }
+     
+    if (isElementPresent) {
+        printf("%d is present in this array.\n", x);
+    } else {
+        printf("%d is not present in this array.\n", x);
+    }
+     
+    return 0;
+}
