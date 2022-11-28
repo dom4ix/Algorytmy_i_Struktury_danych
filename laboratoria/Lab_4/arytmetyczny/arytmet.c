7293b064e60831fc66e59003be54a61e0eb949af
Dominik <47005404+dom4ix@users.noreply.github.com>
2022-11-28 21:47:38 +0200

    Update arytmet.c

--- laboratoria/Lab_4/arytmetyczny/arytmet.c
+++ laboratoria/Lab_4/arytmetyczny/arytmet.c
@@ -1,1 +1,17 @@
+//Dominik Jakubovskis, Student
+//2022-11-28
+#include <math.h>
+#include <stdio.h>
 
+int podajN(int a1, int r, int n){
+  if (n == 1){
+    return a1;
+  } else {
+    return r + podajN(a1, r, n - 1);
+  }
+}
+
+int main(void) {
+  int N = podajN(2, 3, 4);
+  printf("n-ta liczba: %d", N);
+}
