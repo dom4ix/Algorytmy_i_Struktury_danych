6b2fae59460e18230294e06ccceb45655f826e02
Dominik <47005404+dom4ix@users.noreply.github.com>
2022-12-23 21:55:07 +0200

    Create main.c

---
+++ laboratoria/LAB_6/main.c
@@ -0,0 +1,46 @@
+/*
+Dominik Jakubovskis. Student
+* -----------------
+* Include files.
+* -----------------
+2022-12-23
+*/
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int n = 15;
+  int arr[n];
+  FILE *fptr;
+  clock_t start, end;
+  double execution_time;
+
+  // Generujemy random.dat
+  fptr = fopen("random.dat", "w");
+  generate(fptr, n);
+  fclose(fptr);
+
+  // File values to array
+  fptr = fopen("random.dat", "r");
+
+  for (int i = 0; i < n; i++) {
+    fscanf(fptr, "%u", &arr[i]);
+  }
+
+  fclose(fptr);
+
+  int array_size = sizeof(arr) / sizeof(arr[0]);
+
+  start = clock();
+  selectionSort(arr, array_size);
+  end = clock();
+
+  execution_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+  printf("Sorted array: \n");
+  printArray(arr, array_size);
+  printf("\nTime elapsed: %f\n", execution_time);
+  return 0;
+}
